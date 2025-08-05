c05163bc52c907e7577987d72f6729777c57e789
KULDEEP GANGAREKAR <123390998+Kuldeep1246@users.noreply.github.com>
2025-08-06 01:20:48 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"));
 
 app.use("/api/logs", require("./routes/logs"));
